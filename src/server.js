8c16c6e707b6a97bd33635fb7f0927f1a09769a1
Kumar <115116493+developerabhi04@users.noreply.github.com>
2025-07-02 13:42:58 +0530

    lklkl

--- src/server.js
+++ src/server.js
@@ -1,2 +1,2 @@
 // export const server = "http://localhost:4000/api/v1"
-export const server = "https://ananta-tech-serv.vercel.app/api/v1"
+export const server = "https://ananta-techno-backend.vercel.app/api/v1"
